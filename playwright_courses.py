7b978c751d3574b4e730096685cca03eda8a032d
Artem <yatsunenkoaa@fsk.ru>
2025-08-16 11:44:54 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -27,13 +27,20 @@ with sync_playwright() as playwright:
 
     context.storage_state(path="browser-state.json")
 
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
 
     page.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard"
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
     )
 
-    page.wait_for_timeout(5000)
+    courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
+
+    results_title = page.get_by_test_id("courses-list-empty-view-title-text")
+    expect(results_title).to_be_visible()
+    expect(results_title).to_have_text("There is no results")
