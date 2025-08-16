93e9e28718f471cd2cc03059b99ed289c2f66989
Artem <yatsunenkoaa@fsk.ru>
2025-08-16 11:25:54 +0300

    Углубление в Playwright. Практика по углубленной работе с Playwright

---
+++ playwright_error.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import expect, sync_playwright
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
+    )
+
+    # Пытаемся проверить, что несуществующий локатор виден на странице
+    unknown = page.locator("#unknown")
+    expect(unknown).to_be_visible()
