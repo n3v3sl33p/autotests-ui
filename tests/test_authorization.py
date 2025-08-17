43b1470dbac1fdd7611543a9b84988d39fc39967
Artem <yatsunenkoaa@fsk.ru>
2025-08-17 12:50:30 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,37 +1,34 @@
 import pytest
-from playwright.sync_api import expect, sync_playwright
+from playwright.sync_api import Page, expect
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization():
-    with sync_playwright() as playwright:
-        # Открываем браузер и создаем новую страницу
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+def test_wrong_email_or_password_authorization(chromium_page: Page):
+    # Переходим на страницу входа
+    chromium_page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
+    )
 
-        # Переходим на страницу входа
-        page.goto(
-            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
-        )
+    # Заполняем поле email
+    email_input = chromium_page.get_by_test_id("login-form-email-input").locator(
+        "input"
+    )
+    email_input.fill("user.name@gmail.com")
 
-        # Заполняем поле email
-        email_input = page.get_by_test_id("login-form-email-input").locator("input")
-        email_input.fill("user.name@gmail.com")
+    # Заполняем поле пароль
+    password_input = chromium_page.get_by_test_id("login-form-password-input").locator(
+        "input"
+    )
+    password_input.fill("password")
 
-        # Заполняем поле пароль
-        password_input = page.get_by_test_id("login-form-password-input").locator(
-            "input"
-        )
-        password_input.fill("password")
+    # Нажимаем на кнопку Login
+    login_button = chromium_page.get_by_test_id("login-page-login-button")
+    login_button.click()
 
-        # Нажимаем на кнопку Login
-        login_button = page.get_by_test_id("login-page-login-button")
-        login_button.click()
-
-        # Проверяем, что появилось сообщение об ошибке
-        wrong_email_or_password_alert = page.get_by_test_id(
-            "login-page-wrong-email-or-password-alert"
-        )
-        expect(wrong_email_or_password_alert).to_be_visible()
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    # Проверяем, что появилось сообщение об ошибке
+    wrong_email_or_password_alert = chromium_page.get_by_test_id(
+        "login-page-wrong-email-or-password-alert"
+    )
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
