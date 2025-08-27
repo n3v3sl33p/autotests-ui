97f0a86d56ed51d4f5ba4ecd0ce7efd505aac506
Artem <yatsunenkoaa@fsk.ru>
2025-08-27 18:03:06 +0300

    Знакомство с PageObject. Практика работы с Page Object

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import Page, expect
 
 
 @pytest.mark.regression
@@ -12,33 +11,10 @@ from playwright.sync_api import Page, expect
         ("  ", "password"),
     ],
 )
-def test_wrong_email_or_password_authorization(
-    chromium_page: Page, email: str, password: str
-):
-    # Переходим на страницу входа
-    chromium_page.goto(
+def test_wrong_email_or_password_authorization(login_page, email: str, password: str):
+    login_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
     )
-
-    # Заполняем поле email
-    email_input = chromium_page.get_by_test_id("login-form-email-input").locator(
-        "input"
-    )
-    email_input.fill(email)
-
-    # Заполняем поле пароль
-    password_input = chromium_page.get_by_test_id("login-form-password-input").locator(
-        "input"
-    )
-    password_input.fill(password)
-
-    # Нажимаем на кнопку Login
-    login_button = chromium_page.get_by_test_id("login-page-login-button")
-    login_button.click()
-
-    # Проверяем, что появилось сообщение об ошибке
-    wrong_email_or_password_alert = chromium_page.get_by_test_id(
-        "login-page-wrong-email-or-password-alert"
-    )
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
